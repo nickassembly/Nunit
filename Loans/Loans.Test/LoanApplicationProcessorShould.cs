3bb224a2bae0547a1035b91a261881b963ba4cb2
Nick Guerra <nickg@maliciousgames.com>
2020-05-30 15:09:50 -0500

    mock reference types

--- Loans/Loans.Test/LoanApplicationProcessorShould.cs
+++ Loans/Loans.Test/LoanApplicationProcessorShould.cs
@@ -30,6 +30,12 @@ namespace Loans.Tests
          Assert.That(application.GetIsAccepted(), Is.False);
       }
 
+
+      delegate void ValidateCallback(string applicantName,
+                                        int applicantAge,
+                                        string applicantAddress,
+                                        ref IdentityVerificationStatus status);
+
       [Test]
       public void Accept()
       {
@@ -44,6 +50,38 @@ namespace Loans.Tests
                                                65_000);
 
          var mockIdentityVerifier = new Mock<IIdentityVerifier>();
+         // can use It method if you don't need to check against specific values
+         //mockIdentityVerifier.Setup(x => x.Validate(It.IsAny<string>(),
+         //                                      It.IsAny<int>(),
+         //                                      It.IsAny<string>()))
+         //                                     .Returns(true);
+
+         //mockIdentityVerifier.Setup(x => x.Validate("Sarah",
+         //                                    25,
+         //                                    "133 Pluralsight Drive, Draper, Utah"))
+         //                                   .Returns(true);
+
+         //bool isValidOutValue = true;
+         //mockIdentityVerifier.Setup(x => x.Validate("Sarah",
+         //                           25,
+         //                           "133 Pluralsight Drive, Draper, Utah",
+         //                           out isValidOutValue));
+
+         mockIdentityVerifier
+            .Setup(x => x.Validate("Sarah",
+                                    25,
+                                    "133 Pluralsight Drive, Draper, Utah",
+                                    ref It.Ref<IdentityVerificationStatus>.IsAny))
+            .Callback(new ValidateCallback(
+                       (string applicantName,
+                        int applicantAge,
+                        string applicantAddress,
+                        ref IdentityVerificationStatus status) =>
+                                     status = new IdentityVerificationStatus(true)));
+
+
+
+
          var mockCreditScorer = new Mock<ICreditScorer>();
 
          var sut = new LoanApplicationProcessor(mockIdentityVerifier.Object, mockCreditScorer.Object);
@@ -53,6 +91,24 @@ namespace Loans.Tests
          Assert.That(application.GetIsAccepted(), Is.True);
       }
 
+      [Test]
+      public void NullReturnExample()
+      {
+         var mock = new Mock<INullExample>();
 
+         mock.Setup(x => x.SomeMethod()); 
+            // .Returns<string>(null); //mock returns null object by default
+
+         string mockReturnValue = mock.Object.SomeMethod();
+
+         Assert.That(mockReturnValue, Is.Null);
+      }
+
+
+   }
+
+   public interface INullExample
+   {
+      string SomeMethod();
    }
 }
